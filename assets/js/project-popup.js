b5283e2e2d8487fc88bb986e1dd52d788384c238
kiavash <kiavashp@gmail.com>
2019-05-07 07:02:58 -0400

    popup - close button

--- assets/js/project-popup.js
+++ assets/js/project-popup.js
@@ -18,10 +18,13 @@ window.projectPopup = (() => {
     });
 
     return project => {
+        window.closePopup = close;
+
         const popup = document.createElement('div');
         popup.setAttribute('class', 'popup-wrapper');
         popup.innerHTML = `
         <div class="popup">
+            <div class="popup-close" onclick="closePopup()">&#215;</div>
             <div class="popup-title">${project.title}</div>
             <div class="popup-description">${project.description}</div>
             ${project.images.map(image =>
