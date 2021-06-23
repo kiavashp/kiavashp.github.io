24e7c11866c709a803176e023c862ce5c6f7e522
kiavash <kiavashp@gmail.com>
2021-06-23 12:51:28 -0700

    scroll control + back-to links + sort changes

---
+++ docs/scripts/scroll-control.js
@@ -0,0 +1,54 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const scrollProgressElement = document.querySelector('nav:not(.noscroll) > .scroll-progress');
+    const debounceScroll = (fn) => {
+        let frame;
+        let lastreturn;
+        return (...params) => {
+            if (frame) cancelAnimationFrame(frame);
+            frame = requestAnimationFrame(() => lastreturn = fn(lastreturn, ...params));
+        }
+    };
+
+    const isScrolled = (wasScrolled) => {
+        let scrollTop = window.scrollY || document.body.scrollTop || document.documentElement.scrollTop;
+        let scrollHeight = document.body.scrollHeight || document.documentElement.scrollHeight;
+        let viewHeight = window.visualViewport.height;
+
+        if (scrollHeight - viewHeight < 1000) {
+            return false;
+        }
+
+        if (scrollTop > 0 /*400*/) {
+            if (!wasScrolled) {
+                document.body.classList.add('scrolled');
+                wasScrolled = true;
+            }
+        } else if (wasScrolled && scrollTop < 20) {
+            document.body.classList.remove('scrolled');
+            wasScrolled = false;
+        }
+
+        return wasScrolled;
+    }
+
+    const scrollProgress = () => {
+        let scrollTop = window.scrollY || document.body.scrollTop || document.documentElement.scrollTop;
+        let scrollHeight = document.body.scrollHeight || document.documentElement.scrollHeight;
+        let clientHeight = document.documentElement.clientHeight;
+
+        scrollProgressElement.style.width = `${(scrollTop / (scrollHeight - clientHeight)) * 100}%`;
+    };
+
+    if (!scrollProgressElement) {
+        console.log('noscroll');
+        return;
+    }
+
+    document.addEventListener('scroll', debounceScroll(isScrolled), { passive: true });
+
+    document.addEventListener('scroll', debounceScroll(scrollProgress), { passive: true });
+    window.addEventListener('resize', debounceScroll(scrollProgress), { passive: true });
+
+    scrollProgress();
+    isScrolled();
+});
